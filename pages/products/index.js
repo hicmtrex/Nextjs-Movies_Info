57b9ef4b58745df76d4759c07e1c30294fd9ac7e
Firstname Lastname <your_email@youremail.com>
2021-12-24 22:43:45 +0100

    fixed

--- pages/products/index.js
+++ pages/products/index.js
@@ -11,7 +11,7 @@ const ProductsPage = ({ products }) => {
   console.log(productsCtx);
   return (
     <Row>
-      {products.map((movie) => (
+      {productsCtx.map((movie) => (
         <Col key={movie._id} sm={12} md={6} lg={4} xl={3}>
           <Product movie={movie} />
         </Col>
